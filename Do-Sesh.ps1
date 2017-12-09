3902c203d8c073d542a21720eaea7b4cfba964e4
FsckMyLife <aletes010@gmail.com>
2017-12-09 18:18:51 -0500

    Do-Sesh
      <#
      .SYNOPSIS
      Creates new PSSession and enters in one command. Not all that useful.
      .DESCRIPTION
      Literally just new-pssession and enter-pssession rolled into one
      .EXAMPLE
      Do-Sesh -Server hostname
      .EXAMPLE
      Do-Sesh Computer1
      .PARAMETER Server
      The destination accepting your request.
      #>

---
+++ Do-Sesh.ps1
@@ -0,0 +1,33 @@
+﻿function Do-Sesh {
+  <#
+  .SYNOPSIS
+  Creates new PSSession and enters in one command. Not all that useful.
+  .DESCRIPTION
+  Literally just new-pssession and enter-pssession rolled into one
+  .EXAMPLE
+  Do-Sesh -Server hostname
+  .EXAMPLE
+  Do-Sesh Computer1
+  .PARAMETER Server
+  The destination accepting your request.
+  #>
+
+
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory=$True,
+    ValueFromPipeline=$True,
+    ValueFromPipelineByPropertyName=$True,
+      HelpMessage='What computer name would you like to target?')]
+    [Alias('host')]
+    [ValidateLength(3,30)]
+    [string[]]$Server
+  )
+
+  process {
+  New-PSSession $Server| Enter-PSSession
+    }
+
+}
+    
