01b1f033adadfc01ccb3de2624750639b02f5a9a
FsckMyLife <aletes010@gmail.com>
2017-12-09 19:43:24 -0500

    Convert ESD files
    Convert ESD files in public install media to proper WIM format

---
+++ Convert_Win10_Install-ESDtoWIM.ps1
@@ -0,0 +1,7 @@
+﻿#Check image and ascertain target index value
+$SrcPth="X:\Windows\sources"
+dism /Get-WimInfo /WimFile:"$SrcPth\install.esd"
+
+[int]$Index="1"
+
+dism /export-image /SourceImageFile:"$SrcPth\install.esd" /SourceIndex:$Index /DestinationImageFile:"$SrcPth\install.wim" /Compress:max /CheckIntegrity
