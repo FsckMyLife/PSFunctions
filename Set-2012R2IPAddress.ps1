1039afca87c4fd23293e8107bb88495f2e19af39
FsckMyLife <aletes010@gmail.com>
2017-12-09 19:46:55 -0500

    Adding new IP address to 2012 R2 net interface
    #Add conditional handling based on interfacealias format
    #menus to choose adapter?
    #Also make remove, and get part of complete
    #Goal is simplified management of interfaces on core systems

---
+++ Set-2012R2IPAddress.ps1
@@ -0,0 +1,20 @@
+﻿function Set-2012R2IPAddress {
+#2012R2 
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+   [string]$ServerName,
+	
+   [Parameter(Mandatory=$True)]
+   [string]$filePath
+)
+# Here's what you do:
+# Qry interfacealiases from $Server
+invoke-command $Servername {New-NetIPAddress –InterfaceAlias “vEthernet (VS02-Internal)” –IPAddress “10.1.1.25” –PrefixLength 24}
+
+
+}
+
+#2016
+#invoke-command hv05 {New-NetIPAddress –InterfaceAlias “vEthernet (VS02-Internal)” –IPv4Address “10.1.1.27” –PrefixLength 24 -whatif}
+#Lots of things to add here. Can you use menus in functions? I don't think so...
